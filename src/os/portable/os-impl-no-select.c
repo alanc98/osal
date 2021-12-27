762f144c9ee8556a297244a9a13cae51dc063786
Alan Cudmore <alan.cudmore@gmail.com>
2021-12-26 23:40:25 -0500

    RTEMS 6 changes and generic-rtems bsp

---
+++ src/os/portable/os-impl-no-select.c
@@ -0,0 +1,92 @@
+/*
+ *  NASA Docket No. GSC-18,370-1, and identified as "Operating System Abstraction Layer"
+ *
+ *  Copyright (c) 2019 United States Government as represented by
+ *  the Administrator of the National Aeronautics and Space Administration.
+ *  All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/**
+ * \file   os-impl-bsd-select.c
+ * \author joseph.p.hickey@nasa.gov
+ *
+ * Purpose: This file contains wrappers around the select() system call
+ */
+
+/****************************************************************************************
+                                    INCLUDE FILES
+ ***************************************************************************************/
+
+/*
+ * Inclusions Defined by OSAL layer.
+ *
+ * This must include whatever is required to get the prototypes of these functions:
+ *
+ *   FD_SET/FD_CLR/FD_ISSET macros and fd_set typedef
+ *   select()
+ *   clock_gettime() - for computing select timeouts
+ */
+#include <osapi.h>
+#include "os-shared-select.h"
+
+/****************************************************************************************
+                                     DEFINES
+ ***************************************************************************************/
+
+/***************************************************************************************
+                                 FUNCTION PROTOTYPES
+ **************************************************************************************/
+
+/****************************************************************************************
+                                   GLOBAL DATA
+ ***************************************************************************************/
+
+/****************************************************************************************
+                                LOCAL FUNCTIONS
+ ***************************************************************************************/
+
+
+/****************************************************************************************
+                                SELECT API
+ ***************************************************************************************/
+
+
+/*----------------------------------------------------------------
+ *
+ * Function: OS_SelectSingle_Impl
+ *
+ *  Purpose: Implemented per internal OSAL API
+ *           See prototype for argument/return detail
+ *
+ *-----------------------------------------------------------------*/
+int32 OS_SelectSingle_Impl(const OS_object_token_t *token, uint32 *SelectFlags, int32 msecs)
+{
+   return OS_ERR_NOT_IMPLEMENTED;
+} /* end OS_SelectSingle_Impl */
+
+
+/*----------------------------------------------------------------
+ *
+ * Function: OS_SelectMultiple_Impl
+ *
+ *  Purpose: Implemented per internal OSAL API
+ *           See prototype for argument/return detail
+ *
+ *-----------------------------------------------------------------*/
+int32 OS_SelectMultiple_Impl(OS_FdSet *ReadSet, OS_FdSet *WriteSet, int32 msecs)
+{
+   return OS_ERR_NOT_IMPLEMENTED;
+} /* end OS_SelectMultiple_Impl */
+
